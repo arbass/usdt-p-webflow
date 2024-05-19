5e8287c62ff40c950c8b19bc6fd167df5b292b0d
arbass <13ar.bass@gmail.com>
2024-05-19 22:35:05 +0300

    updated: class changer on scroll

--- src/utils/greet.ts
+++ src/utils/greet.ts
@@ -1,18 +1,35 @@
-import { getPublishDate } from '@finsweet/ts-utils';
+export const stepSliderFunc = () => {
+  const stepSlider_el = document.querySelector('[step-slider]');
 
-/**
- * Greets the user by printing a message in the console.
- * @param name The user's name.
- */
-export const greetUser = (name: string) => {
-  const publishDate = getPublishDate();
+  if (stepSlider_el) {
+    //–––
+    function isElementInViewport(el, percentage) {
+      const rect = el.getBoundingClientRect();
+      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+      const elementHeight = rect.height;
+      const threshold = elementHeight * (percentage / 100);
 
-  console.log(`Hello ${name}!`);
-  console.log(
-    `This site was last published on ${publishDate?.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: '2-digit',
-    })}.`
-  );
+      return rect.top >= -threshold && rect.bottom <= windowHeight + threshold;
+    }
+
+    function onScroll() {
+      const elements = document.querySelectorAll('[step-slider-lvl]');
+      const stepItems_elements = document.querySelectorAll('[step-slider-item]');
+
+      elements.forEach((element, id) => {
+        if (isElementInViewport(element, 90)) {
+          stepItems_elements.forEach((stepItem) => {
+            stepItem.classList.remove('is-active');
+          });
+
+          stepItems_elements[id].classList.add('is-active');
+        }
+      });
+    }
+
+    window.addEventListener('scroll', onScroll);
+    window.addEventListener('resize', onScroll); // На случай изменения размера окна
+
+    //–––
+  }
 };
