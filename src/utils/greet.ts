f1dc36b4365b62d6d2ad5995508de07225de47bd
arbass <13ar.bass@gmail.com>
2024-05-20 13:06:35 +0300

    updated: trigger – pc only

--- src/utils/greet.ts
+++ src/utils/greet.ts
@@ -2,47 +2,56 @@ export const stepSliderFunc = () => {
   const stepSlider_el = document.querySelector('[step-slider]');
 
   if (stepSlider_el) {
-    //–––
-    function isElementInViewport(el, percentage) {
-      const rect = el.getBoundingClientRect();
-      const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-      const elementHeight = rect.height;
-      const threshold = elementHeight * (percentage / 100);
+    function myFunction() {
+      if (window.innerWidth >= 992) {
+        //–––
+        function isElementInViewport(el, percentage) {
+          const rect = el.getBoundingClientRect();
+          const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+          const elementHeight = rect.height;
+          const threshold = elementHeight * (percentage / 100);
 
-      return rect.top >= -threshold && rect.bottom <= windowHeight + threshold;
-    }
-
-    function onScroll() {
-      const elements = document.querySelectorAll('[step-slider-lvl]');
-      const stepItems_elements = document.querySelectorAll('[step-slider-item]');
-
-      elements.forEach((element, id) => {
-        if (isElementInViewport(element, 90)) {
-          stepItems_elements.forEach((stepItem) => {
-            stepItem.classList.remove('is-active');
-          });
-
-          stepItems_elements[id].classList.add('is-active');
-
-          //
-
-          function scrollToElement(elementId) {
-            const container = document.getElementById('step-slider_steps-wrapper');
-            const element = document.getElementById(elementId);
-            if (container && element) {
-              container.scrollTop = element.offsetTop - container.offsetTop;
-            }
-          }
-          const stepSelector = 'step-slider_steps-item-' + (id + 1);
-
-          scrollToElement(stepSelector);
+          return rect.top >= -threshold && rect.bottom <= windowHeight + threshold;
         }
-      });
+
+        function onScroll() {
+          const elements = document.querySelectorAll('[step-slider-lvl]');
+          const stepItems_elements = document.querySelectorAll('[step-slider-item]');
+
+          elements.forEach((element, id) => {
+            if (isElementInViewport(element, 90)) {
+              stepItems_elements.forEach((stepItem) => {
+                stepItem.classList.remove('is-active');
+              });
+
+              stepItems_elements[id].classList.add('is-active');
+
+              //
+
+              function scrollToElement(elementId) {
+                const container = document.getElementById('step-slider_steps-wrapper');
+                const element = document.getElementById(elementId);
+                if (container && element) {
+                  container.scrollTop = element.offsetTop - container.offsetTop;
+                }
+              }
+              const stepSelector = 'step-slider_steps-item-' + (id + 1);
+
+              scrollToElement(stepSelector);
+            }
+          });
+        }
+
+        window.addEventListener('scroll', onScroll);
+        window.addEventListener('resize', onScroll); // На случай изменения размера окна
+
+        //–––
+      }
     }
 
-    window.addEventListener('scroll', onScroll);
-    window.addEventListener('resize', onScroll); // На случай изменения размера окна
-
-    //–––
+    window.addEventListener('load', myFunction);
+    window.addEventListener('scroll', myFunction);
+    window.addEventListener('resize', myFunction);
+    myFunction();
   }
 };
